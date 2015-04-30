9d13ef7e84b657813833491014074c272ccef30d
21408116 <21408116@208-02.ens.fsi>
2015-04-30 11:41:41 +0200

    debut

--- projetstri/src/main/java/fr/upstls3/projetstri/Salle.java
+++ projetstri/src/main/java/fr/upstls3/projetstri/Salle.java
@@ -9,5 +9,20 @@ package fr.upstls3.projetstri;
  * @author 21408116
  */
 public class Salle {
+
+    private String nom;
+    private int nbPosteInstalles;
+    private int nbPostePossibles;
+    private String emplacement;
     
+    public Salle (String nom, String emplacement, int nbPostePossi, int nbPosteIns)
+    {
+        this.nom = nom;
+        this.emplacement = emplacement;
+        this.nbPostePossibles = nbPostePossi;
+        this.nbPosteInstalles = nbPosteIns;
+    }
+    
+    Salle s = new Salle("toto", "u2", 50, 10);
+
 }
